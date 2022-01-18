55ce8e8ff8ce4a2eda8c5152c14399c4be80831c
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-01-18 12:33:24 -0500

    Create Invoice and Invoice_Items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,3 +13,19 @@ CREATE TABLE medical_histories (
   status VARCHAR(80),
   CONSTRAINT patient_id_fk FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  CONSTRAINT medical_history_id_fk FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type VARCHAR(80),
+  name VARCHAR(80)
+);
+
