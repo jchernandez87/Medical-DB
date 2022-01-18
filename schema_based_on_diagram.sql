6fa17d6c5b121c0516e7b6da46a1c84e97f159f3
AlexRS90 <aramosbengy@gmail.com>
2022-01-18 11:50:38 -0600

    Add invoice_items table and create prescription as join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -29,3 +29,19 @@ CREATE TABLE treatments (
   name VARCHAR(80)
 );
 
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT invoice_id_FK FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+  CONSTRAINT treatment_id_FK FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
+CREATE TABLE prescription (
+  medical_history_id INT,
+  treatment_id INT,
+  CONSTRAINT medica_FK FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
+  CONSTRAINT treatment_id_FK FOREIGN KEY (treatment_id) REFERENCES treatments (id)
+);
